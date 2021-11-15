88131c6c8c3d6e7bfe3983a9f1a260fba85b419d
Moravec4, Lukas <lukas.moravec4@firma.seznam.cz>
2021-11-15 18:54:35 +0100

    Automaton concatenation

--- regex_to_dfa.hs
+++ regex_to_dfa.hs
@@ -32,7 +32,8 @@ instance Show Regex where
 -- transition functions is a tuple of 3, (current state, input, output state)
 type State = Int
 type Input = Char
-type NGFA = ([Char], [State], [State], [State], [(State, Input, State)])
+type Transition = (State, Input, State)
+type NGFA = ([Char], [State], [State], [State], [Transition])
 
 
 -- There will be needed some helper functions, eg. concatenate automatons
@@ -42,8 +43,14 @@ type NGFA = ([Char], [State], [State], [State], [(State, Input, State)])
 -- From second `y` get rid of input states, keep output states
 -- Add epsilon transition from first `x` output state to second `y` input state 
 
-concatAutomaton:: NGFA -> NGFA -> [Int]
-concatAutomaton (a1, s1, i1, o1, t1) (a2, s2, i2, o2, t2) = [(maximum s1 + 1)..(maximum s2 + length s2)]
+concatAutomaton:: NGFA -> NGFA -> NGFA
+concatAutomaton (a1, s1, i1, o1, t1) (a2, s2, i2, o2, t2) = (
+    a1 ++ a2,
+    s1 ++ [x + (maximum s1 + 1) | x <- s2],
+    i1,
+    [x + (maximum s1 + 1) | x <- o2],
+    t1 ++ fromOutToIn o1 [x + (maximum s1 + 1) | x <- i2] ++ replaceTransitions t2 (maximum s1 + 1)
+    )
 
 orBetweenAutomatons :: NGFA -> NGFA -> NGFA
 orBetweenAutomatons x y = y
@@ -55,12 +62,23 @@ iterateAutomaton x = x
 -- if regex is epsilon, then NGFA is 1 state, that is input and output output, no need for transition functions or whatever
 -- if regex is literal, we need 2 states, 1 transition, from start to finish
 regexToNgfa :: Regex -> NGFA
-regexToNgfa Epsilon = ([], [0], [0], [0], [])
+regexToNgfa Epsilon = (['e'], [0], [0], [0], [])
 regexToNgfa (Lit a) = ([a], [0, 1], [0], [1], [(0, a, 1)])
 regexToNgfa (It x) = iterateAutomaton (regexToNgfa x)
--- regexToNgfa (Con x y) = concatAutomaton (regexToNgfa x) (regexToNgfa y)
+regexToNgfa (Con x y) = concatAutomaton (regexToNgfa x) (regexToNgfa y)
 regexToNgfa (Or x y) = orBetweenAutomatons (regexToNgfa x) (regexToNgfa y)
 regexToNgfa (Grp x) = regexToNgfa x
 
 sample1 = regexToNgfa (Lit 'a')
 sample2 = regexToNgfa (Lit 'b')
+
+
+fromOutToIn :: [State] -> [State] -> [Transition]
+fromOutToIn out inp = [(x, 'e', y) | x <- out, y <- inp]
+
+replaceTransitions :: [Transition] -> Int -> [Transition]
+replaceTransitions t toAdd = [(x + toAdd, y, c + toAdd) | (x, y, c) <- t]
+
+replace :: [Int] -> Int -> Int -> [Int]
+replace [] _ _ = []
+replace (x:xs) a b = if x == a then b : replace xs a b else x : replace xs a b
